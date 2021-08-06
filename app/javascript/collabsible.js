b3b9ac5b0d6f5732263dea5d80c404637ad30608
Ekaterina Novikova <ekaterina.novikova@telefonica.com>
2021-08-06 09:51:06 +0200

    fonts

--- app/javascript/collabsible.js
+++ app/javascript/collabsible.js
@@ -6,6 +6,14 @@ document.addEventListener("turbolinks:load", function () {
 });
 
 // document.addEventListener("turbolinks:load", function () {
+//   $("#add_comment").on("click", function(event) {
+//     event.stopPropagation()
+//     this.parentElement.classList.toggle("active");
+//     $(this).next().slideToggle(300);
+//   }); 
+// });
+
+// document.addEventListener("turbolinks:load", function () {
 //   $(".clickable").click(function() {
 //     window.location = $(this).data("link");
 //   });
